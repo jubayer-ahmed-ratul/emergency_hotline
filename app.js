f4d1fdf620a03c807f0764fd98840600b45c99ba
jubayer-ahmed-ratul <ahmedjubayer.ratul@gmail.com>
2025-08-27 11:45:57 +0600

    call button functionality done

--- app.js
+++ app.js
@@ -26,10 +26,51 @@ for (let i = 0; i < loveIcons.length; i++) {
         // console.log(converted)
 
         const newValue=converted+1;
-        console.log(newValue)
+        
 
         
         loveText.innerText=newValue
       
     });
+}
+
+
+////////////CALL BUTTON
+
+
+
+function callingAlert(name, number) {
+    alert(`CALLING: ${name}\nNUMBER: ${number}`);
+}
+
+
+
+let coinValue=document.getElementById('coin-id');
+
+
+let callButtons=document.getElementsByClassName('call-btn');
+for (const btn of callButtons) {
+
+    btn.addEventListener('click',function(e){
+        e.stopPropagation();
+       
+        let converted=parseInt(coinValue.innerText);
+
+       if (converted>19) {
+       const card = btn.parentNode.parentNode;
+      
+       const name = card.getElementsByTagName('h2')[0].innerText;
+       const number = card.getElementsByTagName('h1')[0].innerText;
+       
+       callingAlert(name,number)
+
+       let callCoin=converted-20;
+       coinValue.innerText=callCoin;
+        
+       }
+       else{
+        alert("insufficient funds")
+       }
+    })
+    
 }
