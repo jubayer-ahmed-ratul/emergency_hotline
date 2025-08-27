cb79630c32174e47f2b7c5ce1c5457c631973619
jubayer-ahmed-ratul <ahmedjubayer.ratul@gmail.com>
2025-08-27 10:26:33 +0600

    love icon functionality

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("js checked");
+//////////PROPERTY CHANGE FUNCTION
+
+
+
+
+
+
+
+
+////////////BUTTON CLICKED FUNCTION
+
+////////////LOVE BUTTON
+
+let loveValue=0;
+let loveIcons = document.getElementsByClassName('love-icon');
+for (let i = 0; i < loveIcons.length; i++) {
+    loveIcons[i].addEventListener('click', function() {
+
+        loveValue++;
+        // console.log(loveValue);
+
+        
+        const loveText=document.getElementById('love-text')
+
+        let converted =parseInt(loveText.innerText)
+        // console.log(converted)
+
+        const newValue=converted+1;
+        console.log(newValue)
+
+        
+        loveText.innerText=newValue
+      
+    });
+}
